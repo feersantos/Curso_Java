53e946e96af10d2ef7cc0b46d83edea17c9bf7ff
feersantos <112442232+feersantos@users.noreply.github.com>
2023-06-14 15:48:32 -0300

    Casting.java

---
+++ Casting.java
@@ -0,0 +1,72 @@
+package Fixacao1;
+
+public class Teste1 {
+
+	public static void main(String[] args) {
+		
+		//////// EXERCICIO 1 ////////
+		
+		int x, y;
+		
+		x = 5;
+		y = 2 * x;
+		
+		System.out.println("Exercício 1");
+		System.out.println(x);
+		System.out.println(y);
+		
+		
+		//////// EXERCICIO 2 ////////
+		
+		//int x; (valor declarado acima)
+		double d;
+		
+		x = 5;
+		d = 2 * x;
+		
+		System.out.println("\nExercício 2");
+		System.out.println(x);
+		System.out.println(d);
+		
+		
+		//////// EXERCICIO 3 ////////
+		
+		double c, C, h, area;
+		
+		c = 6.0;
+		C = 8.0;
+		h = 5.0;
+		
+		area = (c + C) / 2.0 * h;
+		
+		System.out.println("\nExercício 3");
+		System.out.println(area);
+		
+		
+		//////// EXERCICIO 4 ////////
+		
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		resultado = (double) a / b; //adicionar o "(double)" para fazer o casting
+		
+		System.out.println("\nExercício 4");
+		System.out.println(resultado);
+		
+		////////EXERCICIO 5 ////////
+		
+		double j;
+		int k;
+		
+		j = 5.0;
+		k = (int) j;
+		
+		System.out.println("\nExercício 5");
+		System.out.println(k);
+		
+	}
+
+}
