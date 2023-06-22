fdb5ae916c6f57512e6d6e630918bf219889bcf6
feersantos <112442232+feersantos@users.noreply.github.com>
2023-06-22 13:08:32 -0300

    validacao.java

--- validacao.java
+++ validacao.java
@@ -1,39 +1,41 @@
 package Fixacao1;
 
 import java.util.Scanner;
+//import java.util.Locale;
 
 public class exfic {
 
 	public static void main(String[] args) {
 	
+		//Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		 int n;
 		 int in = 0;
 		 int out = 0;
 					 
-	        do {
-	            System.out.print("Digite um valor: ");
-	            while (!sc.hasNextInt()) {
+	     System.out.print("Digite um valor: ");
+	     	while (!sc.hasNextInt()) {
 	                System.out.println("Erro: digite um valor válido.");
 	                sc.next(); // Descarta o valor inválido
-	            }
-	            n = sc.nextInt();
-	        } while (n <= 0);
-		
-
-		for(int i=0; i<n; i++) {
+	        }
+	        n = sc.nextInt();
+	        n = Math.abs(n);
+	        
+	        for(int i=0; i<n; i++) {
 			
 				if(n >= 10 && n <= 20) {
-					in = in + 1;
+					in ++;
 				}
 				else {
-					out = out + 1;
+					out ++;
 				}
 		}
 		
 		System.out.println(in + " in");
 		System.out.println(out + " out");
+		
 		sc.close();
 	}
 }
+	
