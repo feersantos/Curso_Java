2a821351ad54e1b6042ac395056f75c716dbbd14
feersantos <112442232+feersantos@users.noreply.github.com>
2023-09-14 21:46:02 -0300

    Update and rename NotaFacul to NotaFacul.java

--- NotaFacul.java
+++ NotaFacul.java
@@ -1,4 +1,3 @@
-
 package javaapplication1;
 
 /**
@@ -16,50 +15,71 @@ public class NotaFacul {
     public static void main(String[] args){ 
         
     float a1, a2;
-    
+    float a3;
+                
     a1 = 3.0f;
     a2 = 2.5f;
+    a3 = 3.5f;
     
     Aluno aluno; //chamando o metodo
-    aluno = new Aluno();
+    aluno = new Aluno();  
     
-    System.out.println("Média = " + aluno.media());
+    aluno.a1 = 3.0f;
+    aluno.a2 = 2.5f;
+    aluno.a3 = 3.5f;
+    
+    System.out.println("Média = " + aluno.mediaFinal());
     System.out.println("Resultado = " + aluno.resultado());
 
-    
-    
     
 }
     }
 
 
 
-/* METODO
 
+
+/* METODO
 package javaapplication1;
 
 
 public class Aluno {
     
     float a1, a2;
+    float a3;
+    
+    final float MEDIA = 6.0f; //quando é regra de negócio, coloca em maiusculo
     
     //valores iniciais
     {
         a1 = a2 = 0.0f;
+        a3 = 0.0f;
     }
     
     //processo
-    float media(){
+    public float media(){
         return a1 + a2;
     }
     
+    public float maiorNota(){
+        return (a1 > a2) ? a1 : a2; //if
+    }
+    
+    public float mediaFinal(){
+        return (media() > MEDIA) ? media() : a3 + maiorNota();
+    }
+    
     String resultado(){
-        if(media() < 6){
+        if(media() < MEDIA){
+            if(a3 >= 0.0f)
+                return (mediaFinal() < MEDIA)?"Reprovado" : "Aprovado";
           return "Exame";
     } else 
           return "Aprovado";
     }
        
 }
+
+
 
 */
